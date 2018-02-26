a63c7ad8204d2a4a816de8e36c8ca911b1af8943
luyilin <luyilin12@gmail.com>
2018-02-26 23:42:10 +0800

    fix bug

--- src/list.vue
+++ src/list.vue
@@ -21,16 +21,20 @@
                t="1519462199298" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2155" xmlns:xlink="http://www.w3.org/1999/xlink"><defs></defs><path d="M411.485726 111.200056H138.199901a43.350032 43.350032 0 0 0-43.350032 43.350032v273.2786a43.350032 43.350032 0 0 0 43.350032 43.350032h273.285825a43.350032 43.350032 0 0 0 43.350032-43.350032V154.550088a43.350032 43.350032 0 0 0-43.350032-43.350032zM879.384294 111.200056H606.105694a43.350032 43.350032 0 0 0-43.350032 43.350032v273.2786a43.350032 43.350032 0 0 0 43.350032 43.350032h273.2786a43.350032 43.350032 0 0 0 43.350032-43.350032V154.550088a43.350032 43.350032 0 0 0-43.350032-43.350032zM411.485726 554.844281H138.199901a43.350032 43.350032 0 0 0-43.350032 43.350032v273.285825a43.350032 43.350032 0 0 0 43.350032 43.350032h273.285825a43.350032 43.350032 0 0 0 43.350032-43.350032V598.194313a43.350032 43.350032 0 0 0-43.350032-43.350032zM879.384294 554.844281H606.105694a43.350032 43.350032 0 0 0-43.350032 43.350032v273.285825a43.350032 43.350032 0 0 0 43.350032 43.350032h273.2786a43.350032 43.350032 0 0 0 43.350032-43.350032V598.194313a43.350032 43.350032 0 0 0-43.350032-43.350032z" fill="#515151" p-id="2156"></path></svg>
         </h2>
         <div class="examples">
-          <div class="left" v-for="i, index in leftDoc">
-            <demo :expandAll="expandAll" :doc="i"
+          <div class="left">
+            <demo v-for="i, index in leftDoc"
+                  :key="index"
+                  :expandAll="expandAll" :doc="i"
                   :root="root" :highlight="highlight"
                   :slotName="demoIndex('left', index)">
               <slot :name="demoIndex('left', index)"
                     :slot="demoIndex('left', index)"/>
             </demo>
           </div>
-          <div class="right" v-for="i, index in rightDoc">
-            <demo :expandAll="expandAll" :doc="i"
+          <div class="right">
+            <demo v-for="i, index in rightDoc"
+                  :key="index"
+                  :expandAll="expandAll" :doc="i"
                   :root="root" :highlight="highlight"
                   :slotName="demoIndex('right', index)">
               <slot :name="demoIndex('right', index)"
